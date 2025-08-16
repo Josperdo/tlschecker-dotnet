fae59c0796b208033ff1395a2f4aac2477f3bc2c
Scorify <john.sperry1@gmail.com>
2025-08-15 17:25:30 -0700

    TlsProve.cs update

---
+++ src/TlsCheck/TlsProbe.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace TlsCheck;
+
+public static class HttpProbe
+{
+    public static async Task<(bool redirectToHttps, bool httpsReachable, bool hsts, int? status)>
+        CheckAsync(string host, int timeoutMs, CancellationToken ct)
+    {
+        var handler = new SocketHttpHandler
+        {
+            AllowAutoRedirect = false;
+            AutomaticDecompression = DecompressionMethods.None,
+            ConnectTimeout = TimeSpan.FromMilliseconds(timeouMs)
+        };
+        using var client = new HttpClient(handler) { Timeout = TimeSpan.FromMilliseconds(timeoutMs) };
+        
+        bool redirectsToHttps = false;
+        bool httpsReachable = false;
+        bool hsts = false;
+        int? status = null;
+
+        // 1) Check if http:// redirects to https://
+        try
+        {
+            
+        }
+        
+    }
+}
